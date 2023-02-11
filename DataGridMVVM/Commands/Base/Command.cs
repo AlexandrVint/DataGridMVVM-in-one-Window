6daaaaa7eac15e883ea0a2cf3da1c473c9554833
AlexandrVint <alexandr.azizov@gmail.com>
2023-02-11 13:37:54 +0300

    Добавьте файлы проекта.

---
+++ DataGridMVVM/Commands/Base/Command.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace DataGridMVVM.Commands.Base
+{
+    public  abstract class Command:ICommand
+    {
+        // проверка может ли команда выполниться
+        public abstract bool CanExecute(object? parameter);
+
+        // выполнение, если команда может выполниться
+        public abstract void Execute(object? parameter);
+
+        // событие, когда команда выполняется
+        public event EventHandler? CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+    }
+}
---
+++ DataGridMVVM/Commands/LambdaCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataGridMVVM.Commands.Base;
+
+namespace DataGridMVVM.Commands
+{
+    public class LambdaCommand:Command
+    {
+        private readonly Action<object> execute;
+        private readonly Func<object, bool> canExecute;
+
+        public LambdaCommand(Action<object> Execute, Func<object, bool> CanExecute = null)
+        {
+            execute = Execute ?? throw new ArgumentNullException(nameof(Execute));
+            canExecute = CanExecute;
+        }
+
+        public override bool CanExecute(object? parameter)
+        {
+            return canExecute?.Invoke(parameter) ?? true;
+        }
+
+        public override void Execute(object parameter)
+        {
+            execute(parameter);
+        }
+    }
+}
